fb914e4b1e7f564bb2cba496e7b698c3a94e70c6
Renan Ferreira <renanferreira5248@gmail.com>
2022-06-22 18:00:07 -0300

    autorização

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+server.headers() {
+    return(
+        source: '/:path*',
+        heaers:{
+            { key: 'Access-Control-Allow-Credentials', value:'true'},
+            { key: 'Access-Control-Allow-Origin', value:'*'},
+            { key: 'Access-Control-Allow-Methods', value:'GET,OPTIONS,PATCH,DELETE,POST'}
+            { key: 'Access-Control-Allow-Headers', value:'X-CSRF-Token, X-Requested-With'}
+        }
+    )
+}
